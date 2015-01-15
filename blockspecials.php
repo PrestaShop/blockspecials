51f16c07ff9a84501687eda7163b07a3cf1f059e
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-01-15 19:19:28 +0100

    // Version updated to v1.1.4

--- blockspecials.php
+++ blockspecials.php
@@ -38,7 +38,7 @@ class BlockSpecials extends Module
 	{
 		$this->name = 'blockspecials';
 		$this->tab = 'pricing_promotion';
-		$this->version = '1.1.3';
+		$this->version = '1.1.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
